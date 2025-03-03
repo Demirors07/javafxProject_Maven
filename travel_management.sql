081351890f650496a46d0c07cc16614ef6f5d0a0
Demirors07 <D3mirors.07@gmail.com>
2025-03-03 10:03:08 +0300

    travel_management.sql modified ( tel column )

--- travel_management.sql
+++ travel_management.sql
@@ -16,34 +16,12 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: travel_management; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE travel_management WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'Turkish_Türkiye.1254';
-
-
-ALTER DATABASE travel_management OWNER TO postgres;
-
-\connect travel_management
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
--- Name: bus; Type: TABLE; Schema: public; Owner: postgres
+-- Name: bus; Type: TABLE; Schema: public; Owner: myuser
 --
 
 CREATE TABLE public.bus (
@@ -54,10 +32,10 @@ CREATE TABLE public.bus (
 );
 
 
-ALTER TABLE public.bus OWNER TO postgres;
+ALTER TABLE public.bus OWNER TO myuser;
 
 --
--- Name: passenger; Type: TABLE; Schema: public; Owner: postgres
+-- Name: passenger; Type: TABLE; Schema: public; Owner: myuser
 --
 
 CREATE TABLE public.passenger (
@@ -68,10 +46,10 @@ CREATE TABLE public.passenger (
 );
 
 
-ALTER TABLE public.passenger OWNER TO postgres;
+ALTER TABLE public.passenger OWNER TO myuser;
 
 --
--- Name: bus bus_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: bus bus_pkey; Type: CONSTRAINT; Schema: public; Owner: myuser
 --
 
 ALTER TABLE ONLY public.bus
@@ -79,19 +57,11 @@ ALTER TABLE ONLY public.bus
 
 
 --
--- Name: passenger passenger_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: passenger passenger_pkey; Type: CONSTRAINT; Schema: public; Owner: myuser
 --
 
 ALTER TABLE ONLY public.passenger
     ADD CONSTRAINT passenger_pkey PRIMARY KEY (pass_id);
-
-
---
--- Name: passenger passenger_tel_key; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.passenger
-    ADD CONSTRAINT passenger_tel_key UNIQUE (tel);
 
 
 --
